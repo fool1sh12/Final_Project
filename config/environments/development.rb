6ac7a6d026bc0a64343109e4c0f5f24e584ae8be
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-12-08 20:52:59 +0000

    added devise gem, configured local host and mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,11 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { host: 'betterreads.local', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
+
+  config.hosts << "betterreads.local:3000"
+
+
 end

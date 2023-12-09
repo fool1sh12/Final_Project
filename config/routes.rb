2236cbe7c0ac8c99ef4201e221b29dfd5ef186e1
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-12-09 00:26:40 +0000

    added letter_opener and correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+ 
+
   devise_for :users
-  get 'search/index'
   resources :authors
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

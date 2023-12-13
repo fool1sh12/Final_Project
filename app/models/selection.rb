7d37dc32f419dc9b1634833b04853a00f039f784
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-12-13 02:59:24 +0000

    added a rating_show function to display ratings as strings, but keep them as integers in the db, to preform arithmatic with

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -1,4 +1,21 @@
 class Selection < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+
+  def rating_view 
+    if rating == 5
+     "5 stars"
+    elsif rating == 4
+      "4 stars"
+    elsif rating == 3
+      "3 stars"
+    elsif rating == 2
+      "2 stars"
+    elsif rating == 1
+      "1 star"
+    else
+      "Not rated yet!"
+    end
+  end 
 end

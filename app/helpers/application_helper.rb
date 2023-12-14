95f7decaeaec39b1b0d1e1a975c33b1888cf735f
GeorgeK741 <georgiokhachadourian@gmail.com>
2023-12-14 18:22:40 +0000

    Added rating_avg funciton to helpers, and added a rounding option to round avg to 2 decimal places.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def rating_avg
+    total = 0
+    count = 0
+    Selection.all.each do |s|
+      if s.rating?
+        total = total + s.rating
+        count = count + 1
+      end
+    end
+    
+    avg = total.to_f / count 
+    return avg.round(2)
+  end
+        
 end
